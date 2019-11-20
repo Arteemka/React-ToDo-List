5f2f0d5856a9478fd7c49892cc2ba4eb6e6dbafa
Artemka <artemiy.rak@mail.ru>
2019-11-20 16:38:20 +0300

    delete fail js

--- src/components/Button/Button.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Button = props => (
-  <button type="button" className="button-add" onClick={props.listChange}>
-    {props.button}
-  </button>
-);
-
-export default Button;
