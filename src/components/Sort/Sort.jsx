e66c95952be718425f32ec312ebf806f92e56868
Artemka <artemiy@example.com>
2019-11-22 16:04:40 +0300

    add props in component Sort

--- src/components/Sort/Sort.jsx
+++ src/components/Sort/Sort.jsx
@@ -2,23 +2,23 @@ import React from 'react';
 
 import SortField from "../SortField/SortField";
 
-const Sort = () => (
+const Sort = (props) => (
     <>
         <SortField
             nameButton="по возрастанию текста"
             id="button-increase__text"
-            sortField={this.props.sortField} />
+            sortField={props.sortField} />
         <SortField
             nameButton="по убыванию текста"
             id="button-decrease__text"
-            sortField={this.props.sortField} />
+            sortField={props.sortField} />
         <SortField nameButton="по возрастанию даты"
             id="button-increase__date"
-            sortField={this.props.sortField} />
+            sortField={props.sortField} />
         <SortField
             nameButton="по убыванию даты"
             id="button-decrease__date"
-            sortField={this.props.sortField} />
+            sortField={props.sortField} />
     </>
 );
 
